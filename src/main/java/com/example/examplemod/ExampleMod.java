d330e0f215e5e22054d7f960ad617d738c476388
beanflame <68175497+beanflame@users.noreply.github.com>
2021-05-17 12:51:57 +0800

    Update ExampleMod.java

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -6,15 +6,13 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
+public class ExampleMod {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
     
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void init(FMLInitializationEvent event) {
+	// 一些示例代码 DIRT BLOCK（泥土）
+        System.out.println("泥土 >> " + Blocks.dirt.getUnlocalizedName());
     }
 }
